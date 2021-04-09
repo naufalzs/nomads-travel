c97612abf35639329fb45e39fe73da41180dba85
naufalzs <naufal.zufar.s@gmail.com>
2021-04-09 11:25:54 +0700

    Done sistem autentikasi

--- nomads-website/resources/views/includes/navbar.blade.php
+++ nomads-website/resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
-        <a href="#" class="navbar-brand">
+        <a href="{{ route('home') }}" class="navbar-brand">
             <img src="{{ asset('/frontend/images/logo_nomads.png') }}" alt="logo NOMADS" /></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigasi">
             <span class="navbar-toggler-icon"></span>
@@ -29,17 +29,37 @@
                 </li>
             </ul>
 
-            <!-- Mobile Button -->
-            <form class="form-inline d-sm-block d-md-none">
-                <button class="btn btn-login my-2 my-sm-0">Masuk</button>
-            </form>
+            @guest
+                <!-- Mobile Button -->
+                <form class="form-inline d-sm-block d-md-none">
+                    <button class="btn btn-login my-2 my-sm-0" type="button"
+                        onclick="event.preventDefault(); location.href='{{ url('/login') }}';">Masuk</button>
+                </form>
 
-            <!-- Desktop Button -->
-            <form class="form-inline my-2 my-lg-0 d-none d-md-block">
-                <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-                    Masuk
-                </button>
-            </form>
+                <!-- Desktop Button -->
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button"
+                        onclick="event.preventDefault(); location.href='{{ url('/login') }}';">
+                        Masuk
+                    </button>
+                </form>
+            @endguest
+
+            @auth
+                <!-- Mobile Button -->
+                <form class="form-inline d-sm-block d-md-none" action="{{ url('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-login my-2 my-sm-0" type="submit">Logout</button>
+                </form>
+
+                <!-- Desktop Button -->
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block" action="{{ url('logout') }}" method="POST">
+                    @csrf
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+                        Logout
+                    </button>
+                </form>
+            @endauth
         </div>
     </nav>
 </div>
--- nomads-website/routes/web.php
+++ nomads-website/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,6 +20,9 @@ Route::get('/checkout/success', 'CheckoutController@success')->name('success');
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/', 'DashboardController@index')->name('dashboard');
     });
+
+Auth::routes(['verify' => true]);
