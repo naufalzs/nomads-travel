457beccd726f3a2def6378ae42f20e18cf2e2195
naufalzs <naufal.zufar.s@gmail.com>
2021-04-14 15:47:11 +0700

    Done CRUD Travel Package

---
+++ nomads-website/resources/views/pages/admin/travel-package/edit.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.admin')
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Edit Paket Travel {{ $item->title }}</h1>
+        </div>
+
+        <!-- Content Row -->
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="card-shadow">
+            <div class="card-body">
+                <form action="{{ route('travel-package.update', $item->id) }}" method="POST">
+                    @method('PUT')
+                    @csrf
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input class="form-control" type="text" id="title" name="title" placeholder="Title"
+                            value="{{ $item->title }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="location">Location</label>
+                        <input class="form-control" type="text" id="location" name="location" placeholder="Location"
+                            value="{{ $item->location }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="about">About</label>
+                        <textarea name="about" id="about" rows="10"
+                            class="form-control d-block w-100">{{ $item->about }}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="featured_event">Featured Event</label>
+                        <input class="form-control" type="text" id="featured_event" name="featured_event"
+                            placeholder="Featured Event" value="{{ $item->featured_event }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="language">Language</label>
+                        <input class="form-control" type="text" id="language" name="language" placeholder="Language"
+                            value="{{ $item->language }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="foods">Foods</label>
+                        <input class="form-control" type="text" id="foods" name="foods" placeholder="Foods"
+                            value="{{ $item->foods }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="departure_date">Departure Date</label>
+                        <input class="form-control" type="date" id="departure_date" name="departure_date"
+                            placeholder="Departure Date" value="{{ $item->departure_date }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="duration">Duration</label>
+                        <input class="form-control" type="text" id="duration" name="duration" placeholder="Duration"
+                            value="{{ $item->duration }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="type">Type</label>
+                        <input class="form-control" type="text" id="type" name="type" placeholder="Type"
+                            value="{{ $item->type }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="price">Price</label>
+                        <input class="form-control" type="number" id="price" name="price" placeholder="Price"
+                            value="{{ $item->price }}">
+                    </div>
+                    <button class="btn btn-primary btn-block" type="submit">
+                        UBAH
+                    </button>
+                </form>
+            </div>
+        </div>
+        <!-- Content Row -->
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
--- nomads-website/routes/web.php
+++ nomads-website/routes/web.php
@@ -23,6 +23,7 @@ Route::prefix('admin')
     ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/', 'DashboardController@index')->name('dashboard');
+        Route::resource('travel-package', 'TravelPackageController');
     });
 
 Auth::routes(['verify' => true]);
